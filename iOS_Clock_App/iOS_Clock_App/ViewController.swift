4e5b1d55b700b59ebd5a4b246b25b70804145e6a
aaron chen <ixdezbtoeck@gmail.com>
2020-05-26 15:10:54 -0700

    Add IOS Clock App project

---
+++ iOS_Clock_App/iOS_Clock_App/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  iOS_Clock_App
+//
+//  Created by Aaron Chen on 2020-05-26.
+//  Copyright © 2020 Aaron Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /**Just For test now*/
+        view.backgroundColor = .blue
+    }
+
+
+}
+
