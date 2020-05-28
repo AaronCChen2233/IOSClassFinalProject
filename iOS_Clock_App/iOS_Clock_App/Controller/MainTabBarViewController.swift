056eb5b6648c7b02554c64c2e9b14b473e102b79
aaron chen <ixdezbtoeck@gmail.com>
2020-05-28 12:56:35 -0700

    Resolve conflicts

--- iOS_Clock_App/iOS_Clock_App/Controller/MainTabBarViewController.swift
+++ iOS_Clock_App/iOS_Clock_App/Controller/MainTabBarViewController.swift
@@ -14,29 +14,15 @@ class MainTabBarViewController: UITabBarController {
         super.viewDidLoad()
         
         /**Just For test now*/
-<<<<<<< HEAD
-//        view.backgroundColor = .white
-        let firstViewController = StopwatchViewController()
-        firstViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .history, tag: 0)
-
-=======
         
         let firstViewController = ViewController()        
         firstViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
->>>>>>> master
+
         let secondViewController = ViewController()
         secondViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 1)
         let tabBarList = [firstViewController, secondViewController]
         UITabBar.appearance().tintColor = UIColor(named: "highlightOrange")
         viewControllers = tabBarList
-<<<<<<< HEAD
-        
-        UITabBar.appearance().tintColor = colorThem.highlightOrange
-        print(traitCollection.userInterfaceStyle.rawValue)
-        
-=======
->>>>>>> master
-    }
 
-    
+    }
 }
