7bd4e9341f57c68aec730d4392fbb3f4d9f4424a
aaron chen <ixdezbtoeck@gmail.com>
2020-05-28 11:05:49 -0700

    Add them.swift file

---
+++ iOS_Clock_App/iOS_Clock_App/Model/Them.swift
@@ -0,0 +1,19 @@
+//
+//  Them.swift
+//  iOS_Clock_App
+//
+//  Created by Aaron Chen on 2020-05-28.
+//  Copyright © 2020 Aaron Chen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct colorThem {
+    static let highlightOrange = UIColor(red: 255, green: 162, blue: 0)
+    
+}
+
+struct fontSize {
+    static let exampleFontSize = 17
+}
