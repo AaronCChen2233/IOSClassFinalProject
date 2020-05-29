784310099140c8ea06ce62de0a52dd1cf8dbfae8
Naoki Mita <naoki.mita18@gmail.com>
2020-05-28 17:54:08 -0700

    Change text color on tableView by the switch

--- iOS_Clock_App/iOS_Clock_App/AlarmTableViewCell.swift
+++ iOS_Clock_App/iOS_Clock_App/AlarmTableViewCell.swift
@@ -26,16 +26,22 @@ class AlarmTableViewCell: UITableViewCell {
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
         self.textLabel?.font = UIFont.systemFont(ofSize: 45.0)
         self.accessoryView = switchAlarm
+        switchAlarm.addTarget(self, action: #selector(tappedSwitch), for: .valueChanged)
     }
     
     private func updateView() {
         self.textLabel!.text = alarm.dateformat()
         self.detailTextLabel?.text = "\(alarm.label), \(alarm.description())"
         switchAlarm.setOn(alarm.isOn, animated: true)
+        tappedSwitch(switchAlarm)
     }
-
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func tappedSwitch(_ sender : UISwitch!) {
+        self.textLabel?.textColor = switchAlarm.isOn ? .black : .lightGray
+        self.detailTextLabel?.textColor = switchAlarm.isOn ? .black : .lightGray
     }
 }
