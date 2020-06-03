0ab1a6b73264ac20d7484f59a1863dd239f5dc06
aaron chen <ixdezbtoeck@gmail.com>
2020-06-03 15:07:38 -0700

    tabBarItem use system ico

--- iOS_Clock_App/iOS_Clock_App/Controller/MainTabBarViewController.swift
+++ iOS_Clock_App/iOS_Clock_App/Controller/MainTabBarViewController.swift
@@ -17,18 +17,20 @@ class MainTabBarViewController: UITabBarController {
         let rootViewController = WorldClockViewController()
         rootViewController.container = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
         let worldClockViewController = UINavigationController(rootViewController: rootViewController)
-        worldClockViewController.tabBarItem = UITabBarItem(title: "World Clock", image: UIImage(named: ""), selectedImage: UIImage(named: ""))
+        worldClockViewController.tabBarItem = UITabBarItem(title: "Stopwatch", image: UIImage(systemName: "globe"), tag: 0)
+        //worldClockViewController.tabBarItem = UITabBarItem(title: "World Clock", image: UIImage(named: "globe"), selectedImage: UIImage(named: ""))
         
         let AlarmTVC = AlarmTableViewController(style: .grouped)
         AlarmTVC.container = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
         let alarmViewController = UINavigationController(rootViewController: AlarmTVC)
-        alarmViewController.tabBarItem = UITabBarItem(title: "alarm", image: UIImage(named: "alarm_white"), tag: 2)
+//        alarmViewController.tabBarItem = UITabBarItem(title: "alarm", image: UIImage(named: "alarm_white"), tag: 1)
+        alarmViewController.tabBarItem = UITabBarItem(title: "alarm", image: UIImage(systemName: "alarm"), tag: 1)
         
         let stopwatchViewController = StopwatchViewController()
-        stopwatchViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 1)
+        stopwatchViewController.tabBarItem = UITabBarItem(title: "Stopwatch", image: UIImage(systemName: "stopwatch"), tag: 2)
 
         let timerViewController = TimerViewController()
-        timerViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        timerViewController.tabBarItem = UITabBarItem(title: "Timer", image: UIImage(systemName: "timer"), tag: 0)
 
         let tabBarList = [worldClockViewController, alarmViewController, stopwatchViewController, timerViewController]
         UITabBar.appearance().tintColor = UIColor(named: "highlightOrange")
