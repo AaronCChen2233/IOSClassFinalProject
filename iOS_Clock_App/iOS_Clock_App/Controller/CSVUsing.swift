9f5a6c0d8447a890f8f58d081f65343603c0e939
WendaLi <lwd19861127@hotmail.com>
2020-06-08 17:33:27 +0800

    update csv load

--- iOS_Clock_App/iOS_Clock_App/Controller/CSVUsing.swift
+++ iOS_Clock_App/iOS_Clock_App/Controller/CSVUsing.swift
@@ -15,9 +15,11 @@ class CSVUsing {
             let data = try! Data(contentsOf: url)
             let dataEncoded = String(data: data, encoding: .utf8)
             if let rows = dataEncoded?.components(separatedBy: "\n") {
-                for row in rows {
-                    let columns = row.components(separatedBy: ",")
-                    result.append(columns)
+                for (index, row) in rows.enumerated() {
+                    if index != rows.count - 1 {
+                        let columns = row.components(separatedBy: ",").map({$0[1, $0.count - 1]})
+                        result.append(columns)
+                    }
                 }
             }
         }
--- iOS_Clock_App/iOS_Clock_App/Model/timezonedb.csv/readme.txt
+++ iOS_Clock_App/iOS_Clock_App/Model/timezonedb.csv/readme.txt
@@ -7,3 +7,12 @@ Processed by : http://timezonedb.com
 This work is licensed under a Creative Commons Attribution 3.0 License,
 see http://creativecommons.org/licenses/by/3.0/
 The Data is provided "as is" without warranty or any representation of accuracy, timeliness or completeness.
+
+country.csv
+Fields: "country_code","country_name"
+
+timezone.csv
+Fields: "zone_id","abbreviation","time_start","gmt_offset","dst"
+
+zone.csv
+Fields: "zone_id","country_code","zone_name"
